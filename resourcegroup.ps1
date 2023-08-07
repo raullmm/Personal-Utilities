55e8ade095967b0ac0c5f406604e7a7ce60360d6
raullmm <rmorenomunoz@intercept.cloud>
2023-08-07 12:57:22 +0200

    Test completly pipeline

---
+++ resourcegroup.ps1
@@ -0,0 +1,27 @@
+if (Test-Path -Path "$(Build.SourcesDirectory)/Web.zip") {
+    # Expand the Web.zip
+    Expand-Archive -Path "$(Build.SourcesDirectory)/Web.zip" -DestinationPath "./WebExpanded" -Force
+
+    # Get the version
+    $version = Get-Content -Path $(Build.SourcesDirectory)/version                         
+    Write-Host "The version is going to deploy is $version"
+
+    # Rename the file with the specific name
+    Write-Host "The files list are:"
+    ls ".\WebExpanded\"
+    Write-Host "Rename the web.config name with the new version"       
+
+    # Upload the file web.config
+    Write-Host "Upload the file to the correct blob storage:"
+    az storage blob upload --connection-string "$(webconfigStorageAccount)" --type block --file ".\WebExpanded\web.config" --container-name webconfig --name web.config_$version --overwrite
+
+    # Rename the file Web.zip to Portalaks.zip and version to versionaks
+    Write-Host "Rename the file Web.zip to Portalaks.zip and version to versionaks"
+
+    # Upload to the specific container
+    Write-Host "Upload the files Portalaks.zip and versionaks to the correct blob container"
+    az storage blob upload --connection-string "$(webconfigStorageAccount)" --type block --file "$(Build.SourcesDirectory)\Web.zip" --container-name productionportal --name Portalaks.zip --overwrite
+    az storage blob upload --connection-string "$(webconfigStorageAccount)" --type block --file "$(Build.SourcesDirectory)\version" --container-name productionportal --name versionaks --overwrite
+} else {
+    Write-Host "Web.zip not found. Skipping the script."
+}
