33b37c6ec98f6576719b3d5acc75045ac96f0622
raullmm <rmorenomunoz@intercept.cloud>
2023-08-08 11:22:04 +0200

    Add a variable with function Code

--- config_json.ps1
+++ config_json.ps1
@@ -1,10 +1,15 @@
 $version = Get-Content -Path "$env:Build_SourcesDirectory/version" 
-          
-$jsonBody = @{
-  UpdateContainerInstanceviaPipeline = "True"
-  PortalID = "877"
-  WebAppName = "Mavim-nl-scalingdta"
-  Webconfigversion = $version
-  ForceFunction = "True"
-  RedeployApp = "True"
-} | ConvertTo-Json
+
+if (![string]::IsNullOrWhiteSpace($version)) {
+    $jsonBody = @{
+    UpdateContainerInstanceviaPipeline = "True"
+    PortalID = "877"
+    WebAppName = "Mavim-nl-scalingdta"
+    Webconfigversion = "$version"
+    ForceFunction = "True"
+    RedeployApp = "True"
+    } | ConvertTo-Json
+} else {
+    Write-Host "The version variable is empty. The script will not run."
+    Exit 1
+}
